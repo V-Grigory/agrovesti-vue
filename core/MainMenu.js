09bdaed4aac74b332c156a7fa6310bfa25b99e22
Grigory_Volchok <volchok@metrika72.ru>
2021-02-15 15:23:45 +0500

    add template_2 for mainPage

--- core/MainMenu.js
+++ core/MainMenu.js
@@ -18,16 +18,7 @@ class MainMenu {
   }
 
   getMockMainMenu() {
-    let mockMainMenu = [
-      { name_ru: 'Отчеты', name_en: 'test1' },
-      { name_ru: 'Панель исследований', name_en: 'test2' },
-      { name_ru: 'Журнал', name_en: 'test2' },
-      { name_ru: 'Статьи партнеров', name_en: 'test2' },
-      { name_ru: 'Подписка', name_en: 'test2' },
-      { name_ru: 'Контакты', name_en: 'test2' },
-      { name_ru: 'Поиск', name_en: 'test2' }
-    ];
-    return mockMainMenu;
+    return require('../mocks/mainMenu');
   }
 
 }
---
+++ mocks/mainMenu.js
@@ -0,0 +1,11 @@
+const mockMainMenu = [
+  { name_ru: 'Отчеты', name_en: 'test1' },
+  { name_ru: 'Панель исследований', name_en: 'test2' },
+  { name_ru: 'Журнал', name_en: 'test2' },
+  { name_ru: 'Статьи партнеров', name_en: 'test2' },
+  { name_ru: 'Подписка', name_en: 'test2' },
+  { name_ru: 'Контакты', name_en: 'test2' },
+  { name_ru: 'Поиск', name_en: 'test2' }
+];
+
+module.exports = mockMainMenu;
